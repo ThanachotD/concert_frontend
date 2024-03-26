87db32220fc6041394ee9a293aa77c25405951a9
Thanachot W <Thanachot.wongmetin@gmail.com>
2024-03-26 19:37:00 +0700

    build: initial frontend

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center p-24">
+      <div className="flex w-full h-full items-center justify-center">
+        Welcome to HomePage
+      </div>
+    </main>
+  );
+}
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center p-24">
+      <div className="flex w-full h-full items-center justify-center">
+        Welcome to Login Page
+      </div>
+    </main>
+  );
+}
